f0d2c5a44976d21abbc6740441c79792f395b8ab
Brian <brian.e2014@gmail.com>
2017-01-16 13:04:29 -0600

    fixing explosion whoops

--- runner.java
+++ runner.java
@@ -6,9 +6,12 @@ import java.io.UnsupportedEncodingException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Collections;
+
 
 public class runner {
-<<<<<<< HEAD
 	static int DATASET_SIZE;
 	static int ITERATIONS;
 	static int RANK_STARTDO = 300;
@@ -130,37 +133,7 @@ public class runner {
 		System.out.println("Finished, final Dataset");
 		printDataset("out/"+TIMESTAMP);
 	}
-=======
 
-	ArrayList<String> clefs;
-
-	public static void main(String[] args) {
-		//Following should be moved to be taken as arguments
-		int totalParts = 4;
-		ArrayList<ArrayList<Integer>> possibleNotes = new ArrayList<>();
-		ArrayList<String> clefs = new ArrayList<>();
-		ArrayList<String> names = new ArrayList<>(Arrays.asList("Oboe","Marimba","Guitar","Marimba"));
-
-//		ArrayList<String> names = new ArrayList<>(Arrays.asList("Violin 1","Violin 2","Viola","Cello"));
-		
-
-		 possibleNotes.add(new ArrayList<>(Arrays.asList(71,72,74,76)));
-		 possibleNotes.add(new ArrayList<>(Arrays.asList(65,67,71,72)));
-		 possibleNotes.add(new ArrayList<>(Arrays.asList(62,64,67,69)));
-		 possibleNotes.add(new ArrayList<>(Arrays.asList(59,60,64,65)));
-
-		// possibleNotes.add(new ArrayList<>(Arrays.asList(66,69,70,71)));
-		// possibleNotes.add(new ArrayList<>(Arrays.asList(61,63,65,67)));
-		// possibleNotes.add(new ArrayList<>(Arrays.asList(50,52,56,58)));
-		// possibleNotes.add(new ArrayList<>(Arrays.asList(44,46,49,51)));
->>>>>>> c6d17761ac022d3cefbefd3478310c505024793e
-
-//		possibleNotes.add(new ArrayList<>(Arrays.asList(69,72,74,76)));
-//		possibleNotes.add(new ArrayList<>(Arrays.asList(64,65,67,69)));
-//		possibleNotes.add(new ArrayList<>(Arrays.asList(53,55,57,59)));
-//		possibleNotes.add(new ArrayList<>(Arrays.asList(45,48,52,53)));
-
-<<<<<<< HEAD
 	public static Double rank(Melody m){
 		double rank = 0;
 
@@ -278,11 +251,9 @@ public class runner {
 			//RANK_RANGE_REWARD
 			if(cur > 42 && cur < 65) {rank += 5;}else{rank-=RANK_RANGE_REWARD;}
 		}
-=======
->>>>>>> c6d17761ac022d3cefbefd3478310c505024793e
+		return rank;
+	}
 
-
-<<<<<<< HEAD
 	private static int prevNote(int index, Melody m){
 		int ret = index;
 		while(ret>0){
@@ -307,88 +278,4 @@ public class runner {
 		average = average / DATASET_SIZE;
 		log+= ", "+max;
 	}
-=======
-		String  timeStamp = args[0];
->>>>>>> c6d17761ac022d3cefbefd3478310c505024793e
-
-
-
-
-		ArrayList<ArrayList<TempNote>> oneTemp = MovementOne.generate(totalParts);
-		//ArrayList<String> oneParts = tempNotesToParts(oneTemp,possibleNotes);		
-		//buildParts(timeStamp,"I", oneParts,names);
-
-		ArrayList<ArrayList<TempNote>> twoTemp = MovementTwo.generate(totalParts,possibleNotes,TempNote.strip(oneTemp));
-		//ArrayList<String> twoParts = tempNotesToParts(twoTemp,possibleNotes);		
-		//buildParts(timeStamp,"II", twoParts,names);
-		
-
-		for(int i = 0; i < totalParts; i ++){
-			oneTemp.get(i).add(new TempNote("\\bar\"||\" \n \\break \n "));
-			oneTemp.get(i).addAll(twoTemp.get(i));
-		}
-		ArrayList<String> finalParts = tempNotesToParts(oneTemp,possibleNotes);		
-		//System.out.println(finalParts);
-		//System.out.println(oneTemp);
-		buildParts(timeStamp,"III", finalParts,names);
-
-
-	}
-	private static ArrayList<String> tempNotesToParts(ArrayList<ArrayList<TempNote>> tempNotes,
-		ArrayList<ArrayList<Integer>> possibleNotes){
-		ArrayList<String> ret = new ArrayList<>();
-		for(int i = 0; i < tempNotes.size(); i ++){
-			//swap out the temp notes for real ones, then use toString to make it a strin
-			ret.add(Note.toString(TempNote.swap(tempNotes.get(i),possibleNotes.get(i))));
-		}
-		return ret;
-	}	
-
-	private static void buildParts(String timeStamp, String title,
-		ArrayList<String> parts, ArrayList<String> names){
-		try {
-<<<<<<< HEAD
-			PrintWriter writer = new PrintWriter(status+"/melody.ly", "UTF-8");
-			writer.println("#(set-global-staff-size 25)");
-			writer.println("\\header{tagline = \" \"}\n\n\\score{\n\\absolute {");
-			writer.println("\\override Score.BarNumber.break-visibility = ##(#f #f #f) ");
-			writer.println(best.toLilypond());
-			writer.println("\n}\n\\midi{}\n\\layout{}\n}");
-=======
-
-			String res = "";
-
-			File file = new File("templates/Temp1.ly");
-
-	        Scanner sc = new Scanner(file);
-
-	        while (sc.hasNextLine()) {
-	            String i = sc.nextLine();
-	            //System.out.println(i);
-	            if(i.startsWith("%part")){
-	            	i = i.substring(5);
-	            	int index = Integer.parseInt(i);
-	            	res+= parts.get(index) +"\n";
-	            }else if(i.startsWith("%name")){
-	            	i = i.substring(5);
-	            	int index = Integer.parseInt(i);
-	            	res+= names.get(index) +"\n";
-	            }else if(i.startsWith("%timeStamp")){
-	            	res+= timeStamp +"\n";
-	            }
-	            else{
-	            	res+=i + "\n";
-	            }
-	        }
-        	sc.close();
-			PrintWriter writer = new PrintWriter("out/"+timeStamp+"/"+title+".ly", "UTF-8");
-			writer.println(res);
->>>>>>> c6d17761ac022d3cefbefd3478310c505024793e
-			writer.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("I guess we give up now...");
-		}
-	}
-
 }
